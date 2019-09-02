6603bca1c0853e26e12eb4ac50bc07773d2e898d
王月娇 <33644941+wangyuejiao@users.noreply.github.com>
2019-09-02 16:11:52 +0800

    Create helloWorldServer.js

---
+++ helloWorldServer.js
@@ -0,0 +1,28 @@
+const http=require('http');
+
+var server=http.createServer(function(req,res){
+    res.write("hello world");
+    res.end();
+});
+
+server.listen(8081);
+console.log("server is listening 8081");
+
+//编译node.js文件 node 文件名回车
+//每次修改了js文件后，需要重新执行node+文件名
+//shift+鼠标右键 点击打开power shell窗口
+//WebStorm
+
+
+
+//nodeServer.js
+const http=require("http");
+
+var server=new http.Server();
+
+server.on("request",function(req,res){
+    res.end("hello world");
+})
+//server监听端口
+server.listen(8082);
+console.log("server is listening 8082");
